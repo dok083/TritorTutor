601ecf1f22482d4832e504065a64d119f58423e2
Doyoung Kim <dok083ucsd@gmail.com>
2017-01-28 15:06:10 -0800

    NavBar

---
+++ client/app/components/NavBar.js
@@ -0,0 +1,18 @@
+var React = require("react");
+
+var NavBar = React.createClass({
+  render: function () {
+    var pages = ['Sign Up','Log In'];
+    var navLinks = pages.map(function(page){
+      return (
+        <a href={'/' + page}>
+          {page}
+        </a>
+      );
+    });
+
+    return <nav>{navLinks}</nav>;
+  }
+});
+
+module.exports = NavBar;
--- client/app/index.js
+++ client/app/index.js
@@ -2,5 +2,7 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 var App = require("./components/App");
+var NavBar = require("./components/NavBar");
 
 ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<NavBar />, document.getElementById("navbar"));
