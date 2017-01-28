0f5bd2e223ec15311e02c91d5de77bc5e1809398
Natchuta <nwattana@ucsd.edu>
2017-01-28 15:29:45 -0800

    React-bootstrap works

---
+++ client/app/components/MyButton.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var Button = require('react-bootstrap/lib/Button');
+
+var MyButton = React.createClass({
+	render: function() {
+		return <Button>OMG IT WORKS</Button>
+	}
+});
+
+module.exports = MyButton;
