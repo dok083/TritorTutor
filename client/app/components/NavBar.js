0458cf4634930ee941d83182a6b26b68cdaf31a0
Natchuta <nwattana@ucsd.edu>
2017-01-29 10:33:37 -0800

    Dropdown button

--- client/app/components/NavBar.js
+++ client/app/components/NavBar.js
@@ -1,6 +1,8 @@
 var React = require("react");
 var Button = require('react-bootstrap/lib/Button');
-var ButtonToolbar = require('react-bootstrap/lib/ButtonToolbar') ;
+var ButtonToolbar = require('react-bootstrap/lib/ButtonToolbar');
+var DropdownButton = require('react-bootstrap/lib/DropdownButton');
+var MenuItem = require('react-bootstrap/lib/MenuItem');
 
 var NavBar = React.createClass({
    render: function(){
@@ -9,7 +11,15 @@ var NavBar = React.createClass({
          return <Button>{page}</Button>;
       }) ;
 
-      return <ButtonToolbar className="pull-right">{navLinks}</ButtonToolbar> ;
+      return (
+      	<div>
+      	<DropdownButton title="Dropdown" id="bg-nested-dropdown">
+	      <MenuItem eventKey="1">Dropdown link</MenuItem>
+	      <MenuItem eventKey="2">Dropdown link</MenuItem>
+	    </DropdownButton>
+      	<ButtonToolbar className="pull-right">{navLinks}</ButtonToolbar>
+      	</div>
+      );
    }
 });
 
