dc587eb711b57e9015fcd9186622e528489df5f3
Natchuta <nwattana@ucsd.edu>
2017-01-29 10:48:16 -0800

    2-space indentation

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -2,14 +2,14 @@ var React = require("react");
 var NavBar = require("./NavBar");
 
 var App = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<NavBar />
-				<h1>TritonTutor - Test 3</h1>
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div>
+        <NavBar />
+        <h1>TritonTutor - Test 3</h1>
+      </div>
+    );
+  }
 });
 
 module.exports = App;
--- client/app/components/NavBar.js
+++ client/app/components/NavBar.js
@@ -5,22 +5,21 @@ var DropdownButton = require('react-bootstrap/lib/DropdownButton');
 var MenuItem = require('react-bootstrap/lib/MenuItem');
 
 var NavBar = React.createClass({
-   render: function(){
-      var pages = ["Sign Up", "Log In"] ;
-      var navLinks = pages.map(function(page){
-         return <Button>{page}</Button>;
-      }) ;
+  render: function() {
+    var pages = ["Sign Up", "Log In"] ;
+    var navLinks = pages.map(function(page){ 
+    return <Button>{page}</Button>;
+  }) ;
 
-      return (
-      	<div>
-      	<DropdownButton title="Dropdown" id="bg-nested-dropdown">
-	      <MenuItem eventKey="1">Dropdown link</MenuItem>
-	      <MenuItem eventKey="2">Dropdown link</MenuItem>
-	    </DropdownButton>
-      	<ButtonToolbar className="pull-right">{navLinks}</ButtonToolbar>
-      	</div>
-      );
-   }
+  return (
+    <div>
+      <DropdownButton title="Dropdown" id="bg-nested-dropdown">
+        <MenuItem eventKey="1">Dropdown link</MenuItem>
+        <MenuItem eventKey="2">Dropdown link</MenuItem>
+      </DropdownButton>
+      <ButtonToolbar className="pull-right">{navLinks}</ButtonToolbar>
+    </div>
+  )}
 });
 
 module.exports = NavBar;
