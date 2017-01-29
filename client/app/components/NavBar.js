0c57b3e37252fd9f17e281146537abc76bf1b718
Bonnie Chen <boc016@ucsd.edu>
2017-01-29 09:41:46 -0800

    navbar with buttons

--- client/app/components/NavBar.js
+++ client/app/components/NavBar.js
@@ -1,6 +1,9 @@
 var React = require("react");
+var Button = require('react-bootstrap/lib/Button');
+var ButtonToolbar = require('react-bootstrap/lib/ButtonToolbar') ;
 
 var NavBar = React.createClass({
+/*
   render: function () {
     var pages = ['Sign Up','Log In'];
     var navLinks = pages.map(function(page){
@@ -14,6 +17,16 @@ var NavBar = React.createClass({
 
     return <nav>{navLinks}</nav>;
   }
+*/
+
+   render: function(){
+      var pages = ["Sign Up", "Log In"] ;
+      var navLinks = pages.map(function(page){
+         return <Button>{page}</Button>;
+      }) ;
+
+      return <ButtonToolbar className="pull-right">{navLinks}</ButtonToolbar> ;
+   }
 });
 
 module.exports = NavBar;
