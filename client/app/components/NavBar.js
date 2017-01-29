d19825157731731748bdc3b12c63e080846edd7a
Doyoung Kim <dok083ucsd@gmail.com>
2017-01-29 09:51:37 -0800

    clean up

--- client/app/components/NavBar.js
+++ client/app/components/NavBar.js
@@ -1,34 +1,8 @@
 var React = require("react");
 var Button = require('react-bootstrap/lib/Button');
-<<<<<<< HEAD
 var ButtonToolbar = require('react-bootstrap/lib/ButtonToolbar') ;
-=======
->>>>>>> 10194f5090c5e7d2252d5901b63dab2ec0121334
-
 
 var NavBar = React.createClass({
-/*
-  render: function () {
-    var pages = ['Sign Up','Log In'];
-    var navLinks = pages.map(function(page){
-      return (
-        <a href={'/' + page}>
-          {page}
-        </a>
-
-      );
-    });
-
-    return (
-      <div class="container">
-        <div class="row">
-          <nav class="col-6 col-md-4">{navLinks}</nav>
-        </div>
-      </div>
-      )
-  }
-*/
-
    render: function(){
       var pages = ["Sign Up", "Log In"] ;
       var navLinks = pages.map(function(page){
