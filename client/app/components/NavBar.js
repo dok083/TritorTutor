f0c9198041c748cf47bbc6a11e2026d9d5412997
Brian Hang <hangwithbrian@gmail.com>
2017-01-31 12:55:14 -0800

    Revised nav bar to scale

--- client/app/components/NavBar.js
+++ client/app/components/NavBar.js
@@ -3,13 +3,33 @@ var Button = require('react-bootstrap/lib/Button');
 var ButtonToolbar = require('react-bootstrap/lib/ButtonToolbar');
 var DropdownButton = require('react-bootstrap/lib/DropdownButton');
 var MenuItem = require('react-bootstrap/lib/MenuItem');
+var Navbar = require('react-bootstrap/lib/Navbar');
+var FormGroup = require('react-bootstrap/lib/FormGroup');
+var InputGroup = require('react-bootstrap/lib/InputGroup');
+var FormControl = require('react-bootstrap/lib/FormControl');
+var Glyphicon = require('react-bootstrap/lib/Glyphicon');
+
 // for debugging lol
 var Alert = require('react-bootstrap/lib/Alert') ;
 
 var NavBar = React.createClass({
-  handleClick: function(){
+  getInitialState: function(props) {
+    return {isLoggedIn: false};
+  },
+
+  doLogIn: function() {
     // calls NavBarContainer's login flip function
-    this.props.onLog() ;
+
+    // TODO: Make component for log in box and render here.
+    this.setState({isLoggedIn: true});
+  },
+
+  doLogOut: function() {
+    this.setState({isLoggedIn: false});
+  },
+
+  doSignUp: function() {
+
   },
 
   render: function() {
@@ -28,23 +48,51 @@ var NavBar = React.createClass({
     // TODO: nest these inside Navs a react-bootstrap Navbar type
 
     // displays dropdown menu if logged in
-    if(this.props.loggedIn){
-      return (
-        <DropdownButton title="Dropdown" id="bg-nested-dropdown">
+    var loggedIn;
+
+    // Determine what to display in the top right "user" area.
+    if (this.state.isLoggedIn) {
+      loggedIn = (
+        <DropdownButton title="Welcome back!" id="bg-nested-dropdown">
           <MenuItem eventKey="1">Settings</MenuItem>
-          <MenuItem eventKey="2">Log Out</MenuItem>
+          <MenuItem eventKey="2" onClick={this.doLogOut}>Log Out</MenuItem>
         </DropdownButton>
-      ) ;
-    }
-    // displays Log In and Sign Up buttons if logged out
-    else {
-      return (
-        <ButtonToolbar className="pull-right">
-          <Button onClick={this.handleClick}>Log In</Button>
+      );
+    } else {
+      loggedIn = (
+        <ButtonToolbar>
+          <Button onClick={this.doLogIn}><Glyphicon glyph="user" /> Log In</Button>
+          <Button onClick={this.doSignUp}><Glyphicon glyph="globe" /> Sign Up</Button>
         </ButtonToolbar>
-      ) ;
+      );
     }
+
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Tritor</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <InputGroup>
+                <FormControl type="text" placeholder="Looking for a..." />
+                <InputGroup.Button>
+                  <Button type="submit">Search</Button>
+                </InputGroup.Button>
+              </InputGroup>
+            </FormGroup>
+          </Navbar.Form>
+          <Navbar.Form pullRight>
+            {loggedIn}
+          </Navbar.Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
   }
 });
 
-module.exports = NavBar ;
+module.exports = NavBar;
