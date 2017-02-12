28e65146d6f2b3cba71cf24887c0821f28a6ccc9
Doyoung Kim <dok083ucsd@gmail.com>
2017-02-11 16:46:55 -0800

    Footer testing

---
+++ client/app/components/Footer.js
@@ -0,0 +1,15 @@
+var React = require("react") ;
+
+var Footer = React.createClass({
+  render: function () {
+    return (
+      <footer>
+        <img
+          src = "../../../assets/okaysoft.png"
+      </footer>
+    )
+  }
+
+  });
+
+module.exports = Footer;
