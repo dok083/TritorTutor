17567e485d576efb6417d7f0d1ba921206552061
Doyoung Kim <dok083ucsd@gmail.com>
2017-02-11 17:29:45 -0800

    image moved

--- client/app/components/Footer.js
+++ client/app/components/Footer.js
@@ -1,11 +1,14 @@
-var React = require("react") ;
+var React = require("react");
+var img = {
+  src: "../components/okaysoft.png"
+};
 
 var Footer = React.createClass({
   render: function () {
     return (
       <footer>
         <img
-          src = "../../../assets/okaysoft.png"
+          src = {img.src}
         />
       </footer>
     )
