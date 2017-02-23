1b0b1024ee02078c43b5be2e35aabc5659c53926
Aviscer <aviscerality@gmail.com>
2017-02-22 20:09:41 -0800

    Working on Filters

---
+++ client/app/course/TutorPriceFilter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { FormGroup, InputGroup, FormControl, Button, Glyphicon, Panel } from 'react-bootstrap'
+
+class TutorPriceFilter extends React.Component  {
+    render() {
+        return (
+            <Panel header='Filter by Price'>
+                <FormGroup>
+                    
+                </FormGroup>
+            </Panel>
+        );
+    }
+}
+
+TutorPriceFilter.displayName = 'TutorPriceFilter';
+export default TutorPriceFilter
