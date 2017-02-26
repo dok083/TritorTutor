059834349e59d2c75c8d9ed334d4f8bba50ff3cf
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2017-02-25 18:57:43 -0800

    Partially implement messageController and messageModel.

--- server/app/controller/messageController.js
+++ server/app/controller/messageController.js
@@ -18,13 +18,13 @@ var ModelController = {};
  * @param title the title of the message
  * @param content the content of the message
  *
- * @return empty promise
+ * @return promise containing MID
  */
 MessageController.send = function(sender, recipient, title, content)
 {
-    // TODO update database with sender, recipient, title, content
-    // TODO make database generate MID for this message
-    // TODO construct and return empty promise
+    // update database with sender, recipient, title, content and make database 
+    // generate MID for this message; return the promise containing MID
+    return MessageModel.create(sender, recipient, title, content);
 }
 
 /**
@@ -35,11 +35,26 @@ MessageController.send = function(sender, recipient, title, content)
  * @return promise containing a list of the messages associated with the given
  * UID.
  */
-MessageController.get = function(userID) {
-    // TODO retrieve data of all messages with userID
-    // TODO construct a message for every message retrieved
-    // TODO construct list of messages 
-    // TODO construct and return promise containing list of messages
+MessageController.getByUID = function(userID) {
+    // retrieve data of all messages with userID
+    return MessageModel.readUser(userID).then(
+        (results) => {
+            // to hold each message
+            var msgList = [];
+
+            if (results && results.length > 0){
+                // construct and return list of messages 
+                return results.map((result) => {
+                    // add this message to the list
+                    return {
+                        sender: result.sender,
+                        recipient: result.receiver,
+                        title: result.title,
+                        content: result.content
+                    };
+                })
+            }
+        });
 }
 
 /**
@@ -49,7 +64,7 @@ MessageController.get = function(userID) {
  *
  * @return promise containing the message associated with the given MID.
  */
-MessageController.get = function(msgID) {
+MessageController.getByMID = function(msgID) {
     // TODO retrieve data of message with msgID
     // TODO construct a message for this message
     // TODO construct and return promise containing message
