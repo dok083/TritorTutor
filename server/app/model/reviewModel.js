3f1e676ddec6d8747658e9359198261406739365
brianhang <hangwithbrian@gmail.com>
2017-02-28 16:08:26 -0800

    Add Review system

---
+++ server/app/model/reviewModel.js
@@ -0,0 +1,58 @@
+"use strict"
+
+/**
+ * The ReviewModel class is a data access object that allows for storing and
+ * retrieval of reviews in the Tritor database.
+ */
+
+var ReviewModel = {};
+
+var db = require('./database.js');
+
+
+/**
+ * Creates a new review in the database for the user whose ID is userID from the
+ * user whose ID is reviewerID.
+ *
+ * @param userID The ID of the user giving the review.
+ * @param tutorID The ID of the user being reviewed.
+ * @param rating The number of stars 0-5 inclusive for the review.
+ * @param comment A comment for the review.
+ * @return A promise that is called after the review has been made.
+ */
+ReviewModel.create = function(userID, tutorID, rating, comment) {
+    return db.insert('tritor_reviews', {
+        userID: userID,
+        tutorID: tutorID,
+        rating: rating,
+        comment: comment
+    });
+}
+
+/**
+ * Returns a list of reviews from the database for the user whose ID is userID.
+ *
+ * @param userID The ID of the desired user.
+ * @return A promise that contains a list of reviews for the use.r
+ */
+ReviewModel.get = function(userID) {
+    return db.select('tritor_reviews', ['userID', 'rating', 'comment'],
+                     'tutorID=' + userID);
+}
+
+/**
+ * Updates the fields of a specific review in the database. The review is the
+ * review given to the user whose ID is userID from the user whose ID is
+ * reviewerID.
+ *
+ * @param tutorID - The ID of the tutor that was reviewed.
+ * @param userID - The ID of the user that gave the review.
+ * @param values - What to update some fields of the review to.
+ * @return A promise that is called after the review is updated.
+ */
+ReviewModel.update = function(tutorID, userID, values) {
+    return db.update('tritor_reviews', values,
+                     'tutorID=' + userID + ' AND userID='+userID, 1);
+}
+
+module.exports = ReviewModel;
---
+++ server/app/router.js
@@ -0,0 +1,101 @@
+/**
+ * This sets up all of the API routes by loading all of the routes in the routes
+ * subdirectory so files do not need to manually be required.
+ */
+ 
+"use strict"
+
+var express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Length of the .js file extension.
+const JS_EXTENSION = ('.js').length;
+
+/**
+ * Sets up all the routes from some route information to a router so
+ * the requests are properly handled.
+ *
+ * @param routeInfo An object of route URIs and their request callbacks.
+ * @param router The router to use.
+ */
+function bindRoute(routeInfo, router) {
+    // Go through every route and set up request types for it.
+    Object.keys(routeInfo).forEach(function(uri) {
+        // Get which requests are needed for the URI.
+        Object.keys(routeInfo[uri]).forEach(function(request) {
+            router[request](uri, routeInfo[uri][request]);
+        });
+    });
+}
+
+/**
+ * Finds all routes from a given folder and binds them to the given router.
+ *
+ * @param directory The directory that contains routes.
+ * @param callback A function that gets called when a file has been found.
+ * @return An error if the route could not be loaded.
+ */
+function exploreRoutes(directory, callback) {
+    // Find all the .js files and subdirectories.
+    var dirPath = path.join(__dirname, 'routes', directory);
+
+    fs.readdir(dirPath, function(error, routes) {
+        // Ignore this directory if an error occured.
+        if (error) {
+            return error;
+        }
+
+        // Ignore hidden files.
+        routes.forEach(function (route) {
+            if (route[0] == '.') {
+                continue;
+            }
+
+            // If we found a folder, recursively add routes.
+            fs.stat(path + '/' + route, function(error, stat) {
+                if (error) {
+                    return;
+                }
+
+                if (stat.isDirectory()) {
+                    exploreRoutes(directory + '/' + routePath, router);
+                } else if (path.extname(route) == '.js' && callback)  {
+                    callback(path + '/' + route);
+                }
+            });
+        });
+    });
+}
+
+/**
+ * Finds all of the routes within the routes subdirectory and requires them,
+ * passing in the app for the API server so they can do routing.
+ *
+ * @param app The application for the API server
+ */
+function loadRoutes(app) {
+    // Find each route file in the routes subfolder.
+    var routePath = path.join(__dirname, 'routes');
+
+    exploreRoutes(routePath, function(routePath)
+    fs.readdir(routePath, function(err, routes) {
+        routes.forEach(function(route) {
+            // Only load JavaScript files.
+            if (path.extname(route) !== '.js') {
+                return;
+            }
+
+            // Set up middleware to handle our API routes.
+            var router = express.Router();
+
+            // Set up the router so handle the specified routes.
+            var routeInfo = require("./routes/" + route);
+            bindRoute(routeInfo, router);
+
+            app.use('/' + route.substr(0, route.length - JS_EXTENSION), router);
+        });
+    });
+}
+
+module.exports = loadRoutes;
