e51fda19fe47c79fc8bdb2e8e9ed84277c446dbc
brianhang <hangwithbrian@gmail.com>
2017-03-10 17:31:59 -0800

    Start on tutor update

--- client/app/course/CourseTutorRequest.js
+++ client/app/course/CourseTutorRequest.js
@@ -9,43 +9,39 @@ class CourseTutorRequest extends React.Component {
   constructor(props) {
     super(props);
 
+    const tutorInfo = props.tutorInfo;
+
     this.state = {
-      user: null,
-      description: '',
-      price: '',
-      negotiable: false,
+      description: tutorInfo ? tutorInfo.description : '',
+      price: tutorInfo ? tutorInfo.price : 0,
+      negotiable: tutorInfo ? Boolean(tutorInfo.negotiable) : false,
       message: '',
       messageType: '',
       busy: false
     }
   }
 
-  onHide() {
-      // Reset the states when hidden.
-      this.setState({
-          message: '',
-          messageType: '',
-          busy: false
-      });
+  componentWillReceiveProps(props) {
+    const tutorInfo = props.tutorInfo;
 
-      if (this.props.onHide) {
-          this.props.onHide();
-      }
+    this.setState({
+      description: tutorInfo ? tutorInfo.description : '',
+      price: tutorInfo ? tutorInfo.price : 0,
+      negotiable: tutorInfo ? Boolean(tutorInfo.negotiable) : false,
+    });
   }
 
-  componentWillMount() {
-    // Get who we are currently logged in as.
-    Dispatch.addListener('getUserInfo', (data) => {
-      if (data.component == this) {
-        this.setState({
-          user: data.user,
-        });
-      }
+  onHide() {
+    // Reset the states when hidden.
+    this.setState({
+        message: '',
+        messageType: '',
+        busy: false
     });
 
-    var action = Dispatch.createAction('requestUserInfo');
-    action.set('component', this);
-    action.dispatch();
+    if (this.props.onHide) {
+      this.props.onHide();
+    }
   }
 
   onDescriptionChange(e) {
@@ -60,8 +56,8 @@ class CourseTutorRequest extends React.Component {
     this.setState({negotiable: !this.state.negotiable});
   }
 
-  submit() {
-    var price = parseInt(this.state.price);
+  getValues() {
+    var price = parseFloat(this.state.price).toFixed(2);
 
     if (!price || price < 0) {
       this.setState({
@@ -74,11 +70,70 @@ class CourseTutorRequest extends React.Component {
 
     this.setState({busy: true});
 
-    var listing = {
+    return {
         desc: this.state.description,
         price: this.state.price,
         nego: this.state.negotiable
     };
+  }
+
+  update() {
+    var listing = this.getValues();
+
+    if (!listing) {
+      return;
+    }
+
+    var changes = {};
+
+    Object.keys(listing).forEach((key) => {
+      if (this.props.tutorInfo[key] != listing[key]) {
+        changes[key] = listing[key];
+      }
+    });
+
+    if (changes.length == 0) {
+      this.setState({
+        message: 'There is nothing to update.',
+        messageType: 'warning'
+      });
+
+      return;
+    }
+
+    axios.put('/api/tutor/' + this.props.course, changes)
+      .then(() => {
+        this.setState({
+          message: 'Your tutoring listing has been updated.',
+          messageType: 'success',
+          busy: false
+        });
+      })
+      .catch((error) => {
+        var message = error.response && error.response.data;
+
+        if (message.message) {
+          message = message.message;
+        } else {
+          message = error.toString();
+        }
+
+        this.setState({
+          message: message,
+          messageType: 'danger',
+          busy: false
+        });
+      });
+  }
+
+  submit() {
+    this.setState({busy: true});
+
+    var listing = this.getValues();
+
+    if (!listing) {
+      return;
+    }
 
     axios.post('/api/tutor/' + this.props.course, listing)
       .then(() => {
@@ -102,12 +157,12 @@ class CourseTutorRequest extends React.Component {
   }
 
   render() {
-    if (!this.state.user) {
+    if (!this.props.user) {
       return <p className='text-center'>Loading</p>;
     }
 
     // Show an error if the user is not verified.
-    if (this.state.user.verified == false) {
+    if (this.props.user.verified == false) {
       return <p className='text-center'>Sorry, you must be verified to use this feature.</p>;
     }
 
@@ -116,6 +171,8 @@ class CourseTutorRequest extends React.Component {
     if (this.state.message.length > 0) {
       message = <Alert bsStyle={this.state.messageType}>{this.state.message}</Alert>;
     }
+
+    const buttonText = this.props.tutorInfo ? 'Update Listing' : 'Add Listing';
 
     return (
       <Modal show={this.props.show} onHide={this.onHide.bind(this)}>
@@ -127,24 +184,30 @@ class CourseTutorRequest extends React.Component {
           <form>
             <FormGroup controlId='offer'>
               <ControlLabel>Offer Description</ControlLabel>
-              <FormControl type='text' onChange={this.onDescriptionChange.bind(this)} />
+              <FormControl type='text' onChange={this.onDescriptionChange.bind(this)}
+                           defaultValue={this.state.description} />
             </FormGroup>
 
             <FormGroup controlId='price'>
               <ControlLabel>Price per Lesson</ControlLabel>
               <InputGroup>
                 <InputGroup.Addon>$</InputGroup.Addon>
-                <FormControl type='text' onChange={this.onPriceChange.bind(this)} />
+                <FormControl type='text' onChange={this.onPriceChange.bind(this)}
+                                         defaultValue={this.state.price} />
               </InputGroup>
             </FormGroup>
 
             <FormGroup controlId='negotiable'>
-              <Checkbox onChange={this.onNegotiableChange.bind(this)} inline>Negotiable pricing</Checkbox>
+              <Checkbox onChange={this.onNegotiableChange.bind(this)}
+                        defaultChecked={this.state.negotiable}
+                        inline>Negotiable pricing</Checkbox>
             </FormGroup>
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button type='submit' bsStyle='primary' onClick={this.submit.bind(this)} disabled={this.state.busy}>Submit</Button>
+          <Button type='submit' bsStyle='primary'
+                  onClick={this.props.tutorInfo ? this.update.bind(this) : this.submit.bind(this)}
+                  disabled={this.state.busy}>{buttonText}</Button>
         </Modal.Footer>
       </Modal>
     );
