bc51d59d63e112af0cf73fae0ddbddabd59a7725
brianhang <hangwithbrian@gmail.com>
2017-03-11 12:14:09 -0800

    Finish course search bar

--- client/app/course/CourseSearch.js
+++ client/app/course/CourseSearch.js
@@ -1,17 +1,53 @@
 import React from 'react'
 import { Grid, Col } from 'react-bootstrap'
+import axios from 'axios'
 
-import CourseListContainer from './CourseListContainer'
+import CourseListComponent from './CourseListComponent'
 
 class CourseSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      results: []
+    };
+  }
+
+  componentWillMount() {
+    axios.get('/api/course/search/' + this.props.params.query)
+      .then((results) => {
+        this.setState({results: results.data});
+      });
+  }
+
+  componentWillReceiveProps(props) {
+    axios.get('/api/course/search/' + props.params.query)
+      .then((results) => {
+        this.setState({results: results.data});
+      });
+    
+  }
+
   render() {
+    var courseList = this.state.results;
+
+    courseList.sort((a, b) => {
+      if (a.classID.length == b.classID.length) {
+        return a.classID.localeCompare(b.classID);
+      }
+
+      return a.classID.length - b.classID.length;
+    });
+
+    var results = courseList.map((result, index) => {
+      return <CourseListComponent course={result.classID} key={index} />;
+    });
+
     return (
       <div id='container'>
         <Grid>
-          <h1>Search results for {this.props.params.query}</h1>
-          <Col>
-            <CourseListContainer />
-          </Col>
+          <h2>Search results for &quot;{this.props.params.query}&quot;</h2>
+          {results}
         </Grid>
       </div>
     )
