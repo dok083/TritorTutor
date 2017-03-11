8e79941a66ee4cda24e189df4ccc91bdc32d7f0b
Natchuta <nwattana@ucsd.edu>
2017-03-10 16:22:58 -0800

    Add send message button and modal

--- client/app/profile/RequestComponent.js
+++ client/app/profile/RequestComponent.js
@@ -1,33 +1,28 @@
 import React from 'react'
-import { Modal,Select, FormControl, Button, Panel} from 'react-bootstrap'
+import { Modal, Select, FormControl, Button } from 'react-bootstrap'
 
 class RequestComponent extends React.Component {
   render() {
-    return (
-          
-          <Panel>
+    return (    
+      <div>
+        <Modal.Body>
+          <p>Choose a course that you wanted to be tutored from {this.props.user.username}</p>
 
-            <Modal.Body>
-              <p>Choose a course that you wanted to be tutored from {this.props.user.username}</p>
+          <FormControl componentClass="select">
+            <option value="-1" disabled>--</option>
+            <option value="1">one</option>
+            <option value="2">two</option>
+            <option value="3">three</option>
+          </FormControl>
 
-              <FormControl componentClass="select">
-    		        <option value="-1" disabled>--</option>
-    		        <option value="1">one</option>
-    	      	  <option value="2">two</option>
-   		          <option value="3">three</option>
-  	          </FormControl>
-              <br></br>
-
-              <Button onClick={this.close} className="pull-right" bsStyle ="primary">
-                    Request Your Tutor
-                  </Button>
-
-            </Modal.Body>
-
-          </Panel>
-
-               
-      
+          <br></br>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.close} className="pull-right" bsStyle ="primary">
+            Request Your Tutor
+          </Button>
+        </Modal.Footer>
+      </div>
     );
   }
 }
