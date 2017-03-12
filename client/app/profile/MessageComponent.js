5d733fbff750f33104a603ab2a5a8c248f056896
Aviscer <aviscerality@gmail.com>
2017-03-11 20:47:45 -0800

    Message send

--- client/app/profile/MessageComponent.js
+++ client/app/profile/MessageComponent.js
@@ -2,22 +2,70 @@ import React from 'react'
 import { Modal, Select, FormControl, Button, FormGroup, ControlLabel } from 'react-bootstrap'
 
 class MessageComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      subject: "",
+      content: "",
+      busy: false,
+      message: '',
+      messageType: 'danger'
+    };
+  }
+
+  subjectChange(e) {
+    this.setState({subject: e.target.value});
+  }
+
+  contentChange(e) {
+    this.setState({content: e.target.value});
+  }
+
+  send() {
+    this.setState({busy: true});
+    axios.post('/api/message/' + this.props.user.userID, {
+      subject: this.state.subject,
+      content: this.state.content
+    })
+      .then(()=> {
+        this.setState({
+          message: 'Your message has been sent.',
+          messageType: 'success'
+        });
+      }).catch((error) => {
+        var message = error.response && error.response.data;
+
+        if (message && message.message) {
+          message = message.message;
+        } else {
+          message = error.toString();
+        }
+
+        this.setState({
+          message: message,
+          messageType: 'danger',
+          busy: false
+        });
+      });
+  }
+
   render() {
     return (    
       <div>
         <Modal.Body>
           <FormGroup id="subject" key="subject">
             <ControlLabel>Subject</ControlLabel>
-            <FormControl type="text" placeholder="Subject" required />
+            <FormControl onChange={this.subjectChange.bind(this)} type="text" placeholder="Subject" required />
           </FormGroup>
           <FormGroup>
             <ControlLabel>Content</ControlLabel>
-            <FormControl componentClass="textarea" placeholder="Content" rows={5}/>
+            <FormControl onChange={this.contentChange.bind(this)} componentClass="textarea" placeholder="Content" rows={5}/>
           </FormGroup>
         </Modal.Body>
                          
         <Modal.Footer>
-          <Button className="pull-right" onClick="" bsStyle ="primary">
+          <Button className="pull-right" onClick={this.bind.send.bind(this)} bsStyle ="primary" disable={this.state.busy}>
             Send
           </Button>
         </Modal.Footer>
