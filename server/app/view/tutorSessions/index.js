f42dee8693c9f6ab3a9458259d7e9b6cb7d1fb9b
Amal Alhaidari <amal.alhaidari@gmail.com>
2017-03-13 00:45:47 -0700

    Fix indentation

--- server/app/view/tutorSessions/index.js
+++ server/app/view/tutorSessions/index.js
@@ -193,138 +193,135 @@ function getSessionsWith(req, res, user) {
 		    res.json(filterSessions);
         });
 }
-//TODO
-    /**
+
+/**
   * Ends a session from a given session ID.
   */
- function sessionFinish(req, res, user) {
-if (!user.verified) {
-return res.status(400).json({message: 'You are not verified'});
+function sessionFinish(req, res, user) {
+    if (!user.verified) {
+	return res.status(400).json({message: 'You are not verified'});
+    }
+
+    var sessionID = req.params.id;
+
+    // Get the session the user wants.
+    TutorSessionController.getByID(sessionID)
+	.then((session) => {
+	    if(session.tutorID == user.userID) {
+		var isTutor = true;
+		var isStudent = false;
+	    } else if (session.studentID == user.userID){
+		var isTutor = false;
+		var isStudent = true;
+	    }
+
+	    //session exists and status of tutoring is ongoing and user is either student or tutor
+	    if (session && session.status == 1 && (isTutor || isStudent)) {
+	    TutorSessionController.update(session.sessionID, 2)
+		.then(()=> {
+		    if (isTutor){       //the user is a tutor (can get username), get student's username
+			ProfileController.get(session.studentID)
+			    .then((studentProfile)=>{
+				var studentUsername = studentProfile.username;
+
+				var tutorMsg = "Your session with [" + studentUsername + "](https://tritontutor.com/profile/" + session.tutorID  + ") for ["
+						+ session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been canceled."
+
+				var studentMsg = "Your session with [" + user.username + "](https://tritontutor.com/profile/" + session.tutorID  + ") for ["
+						+ session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been canceled."
+
+				MessageController.send(0, session.studentID, 'Session ended', studentMsg);
+				MessageController.send(0, session.tutorID, 'Session ended', tutorMsg);
+				res.json({message: 'success'});
+			    }); 
+		    } else if (isStudent) {     //the user is a student (can get username), get tutor's username
+			ProfileController.get(session.tutorID)
+			    .then((tutorProfile)=>{ 
+				var tutorUsername = tutorProfile.username;
+				
+				var tutorMsg = "Your session with student [" + user.username + "](https://tritontutor.com/profile/" + session.studentID  + ") for ["
+						+ session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been canceled." 
+
+				var studentMsg = "Your session with [" + tutorUsername + "](https://tritontutor.com/profile/" + session.tutorID  + ") for ["
+						+ session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been canceled."
+
+				MessageController.send(0, session.studentID, 'Session ended', studentMsg);
+				MessageController.send(0, session.tutorID, 'Session ended', tutorMsg);
+				res.json({message: 'success'});
+			    });
+		    }
+		});
+	    } else {
+		res.status(400).json({message: 'The tutor session could not be found.'});
+	    }
+	});
 }
 
-var sessionID = req.params.id;
-
-// Get the session the user wants.
-TutorSessionController.getByID(sessionID)
-.then((session) => {
-if(session.tutorID == user.userID) {
-var isTutor = true;
-var isStudent = false;
-} else if (session.studentID == user.userID){
-var isTutor = false;
-var isStudent = true;
-}
-
-//session exists and status of tutoring is ongoing and user is either student or tutor
-if (session && session.status == 1 && (isTutor || isStudent)) {
-TutorSessionController.update(session.sessionID, 2)
-.then(()=> {
-if (isTutor){       //the user is a tutor (can get username), get student's username
-ProfileController.get(session.studentID)
-.then((studentProfile)=>{
-var studentUsername = studentProfile.username;
-
-var tutorMsg = "Your session with [" + studentUsername + "](https://tritontutor.com/profile/" + session.tutorID  + ") for ["
-+ session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been canceled."
-
-var studentMsg = "Your session with [" + user.username + "](https://tritontutor.com/profile/" + session.tutorID  + ") for ["
-+ session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been canceled."
-
-MessageController.send(0, session.studentID, 'Session ended', studentMsg);
-MessageController.send(0, session.tutorID, 'Session ended', tutorMsg);
-res.json({message: 'success'});
-}); 
-} else if (isStudent) {     //the user is a student (can get username), get tutor's username
-ProfileController.get(session.tutorID)
-.then((tutorProfile)=>{ 
-var tutorUsername = tutorProfile.username;
-
-var tutorMsg = "Your session with student [" + user.username + "](https://tritontutor.com/profile/" + session.studentID  + ") for ["
-+ session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been canceled." 
-
-var studentMsg = "Your session with [" + tutorUsername + "](https://tritontutor.com/profile/" + session.tutorID  + ") for ["
-+ session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been canceled."
-
-MessageController.send(0, session.studentID, 'Session ended', studentMsg);
-MessageController.send(0, session.tutorID, 'Session ended', tutorMsg);
-res.json({message: 'success'});
-});
-}
-});
-} else {
-res.status(400).json({message: 'The tutor session could not be found.'});
-}
-});
-}
-//TODO
- /* Allows a user to give a response to a session request.
+/* Allows a user to give a response to a session request.
  */
- function requestSessionResponse(req, res, user) {
- if (!user.verified) {
- return res.status(400).json({message: 'You are not verified'});
+function requestSessionResponse(req, res, user) {
+	if (!user.verified) {
+	    return res.status(400).json({message: 'You are not verified'});
+	}
+	var studentID = req.params.id;
+	var tutorID = user.userID;
+	var accept = Boolean(req.body.accept);
+	
+	// Get this username of the student
+	ProfileController.get(studentID)
+		.then((studentProfile) => {
+			var studentUsername = studentProfile.username;
+			// Find a pending session with the studentID and courseID.
+			// If one does not exist, then error.
+			TutorSessionController.getBetween(studentID, tutorID)
+				.then((results)=> {
+					var session;
+
+					for (var i = 0; i < results.length; i++) {
+						if (results[i].status == 0) {
+							session = results[i];
+
+							break;
+						}
+					}
+					if (!session) {
+						return res.status(400).json({message: 'You do not have a pending request from this user'});
+					}
+					console.log(session)
+					// Otherwise, set the session state to active if accept is true or                                                                                                                                                         81,0-1
+					// delete the request if false.
+					// Send a message to the student with the result of the response.
+					// Send a message to the tutor indicate the response.
+					if (accept) {
+						TutorSessionController.update(session.sessionID, 1)
+							.then(()=> {
+								var tutorMsg = "You accepted a tutoring request from [" + studentUsername + "](https://tritontutor.com/profile/"
+										+ studentID + ") for [" + session.classID + "](https://tritontutor.com/course/" + session.classID + ").";
+						 		var studentMsg = "Your request to be tutored by [" + user.username + "](https://tritontutor.com/profile/" + tutorID
+										+ ") for [" + session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been accepted.";
+							MessageController.send(0, studentID, user.username + " accepted your request", studentMsg);
+                                 			MessageController.send(0, tutorID, 'You accepted a request', tutorMsg);
+ 
+                                 			res.json({message: 'success'});
+                             				});
+                     			} else {
+                         			TutorSessionController.update(session.sessionID, -1)
+                             				.then(()=> {
+                                 				var tutorMsg = "You rejected a tutoring request from [" + studentUsername + "](https://tritontutor.com/profile/"
+                                                				+ studentID + ") for [" + session.classID + "](https://tritontutor.com/course/" + session.classID + ").";
+                                 				var studentMsg = "Your request to be tutored by [" + user.username + "](https://tritontutor.com/profile/" + tutorID
+						  				+ ") for [" + session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been rejected.";
+ 
+                                 				MessageController.send(0, studentID, user.username + ' rejected your request', studentMsg);
+                                 				MessageController.send(0, tutorID, 'You rejected a request', tutorMsg);
+ 
+                                 				res.json({message: 'success'});
+                             				});
+                     			}
+                 		});
+         	});
  }
- 
-var studentID = req.params.id;
- var tutorID = user.userID;
- var accept = Boolean(req.body.accept);
- 
- // Get this username of the student
- ProfileController.get(studentID)
- .then((studentProfile) => {
- var studentUsername = studentProfile.username;
- // Find a pending session with the studentID and courseID.
- // If one does not exist, then error.
-TutorSessionController.getBetween(studentID, tutorID)
-.then((results)=> {
-var session;
 
-for (var i = 0; i < results.length; i++) {
-if (results[i].status == 0) {
-session = results[i];
-
-break;
-}
-}
-
-if (!session) {
-return res.status(400).json({message: 'You do not have a pending request from this user'});
-}
-console.log(session)
-// Otherwise, set the session state to active if accept is true or                                                                                                                                                         81,0-1
-// delete the request if false.
-// Send a message to the student with the result of the response.
-// Send a message to the tutor indicate the response.
-if (accept) {
-                         TutorSessionController.update(session.sessionID, 1)
-                             .then(()=> {
- 
-                                 var tutorMsg = "You accepted a tutoring request from [" + studentUsername + "](https://tritontutor.com/profile/"
-                                                + studentID + ") for [" + session.classID + "](https://tritontutor.com/course/" + session.classID + ").";
-                                 var studentMsg = "Your request to be tutored by [" + user.username + "](https://tritontutor.com/profile/" + tutorID
-						+ ") for [" + session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been accepted.";
- 
-                                 MessageController.send(0, studentID, user.username + " accepted your request", studentMsg);
-                                 MessageController.send(0, tutorID, 'You accepted a request', tutorMsg);
- 
-                                 res.json({message: 'success'});
-                             });
-                     } else {
-                         TutorSessionController.update(session.sessionID, -1)
-                             .then(()=> {
-                                 var tutorMsg = "You rejected a tutoring request from [" + studentUsername + "](https://tritontutor.com/profile/"
-                                                + studentID + ") for [" + session.classID + "](https://tritontutor.com/course/" + session.classID + ").";
-                                 var studentMsg = "Your request to be tutored by [" + user.username + "](https://tritontutor.com/profile/" + tutorID
-						  + ") for [" + session.classID + "](https://tritontutor.com/course/" + session.classID + ") has been rejected.";
- 
-                                 MessageController.send(0, studentID, user.username + ' rejected your request', studentMsg);
-                                 MessageController.send(0, tutorID, 'You rejected a request', tutorMsg);
- 
-                                 res.json({message: 'success'});
-                             });
-                     }
-                 });
-         });
- }
 module.exports = {
 	'/:id': {
         get: requiresLoggedIn(getSessionsWith),
